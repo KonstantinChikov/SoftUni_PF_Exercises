600dc43f336f8a9bbf16ec49e66744fce7ac1e0c
KonstantinChikov <kmpetrov7@gmail.com>
2023-10-22 00:11:18 +0300

    Exercises on Arrays Advanced

---
+++ Chapter_XVIII/Train.js
@@ -0,0 +1,29 @@
+// Ex. I
+
+function train(arr){
+    let wagons = arr[0].split(' ').map(Number);
+    let capacity = Number(arr[1]);
+
+    for(let i = 2; i < arr.length; i++){
+        let command = arr[i];
+        let token = command.split(' ');
+
+        if(token[0] == 'Add'){
+            let passangers = Number(token[1]);
+            wagons.push(passangers);
+        }else{
+            let passangers = Number(token[0]);
+
+            for(let index = 0; index < wagons.length; index++){
+                if(wagons[index] + passangers <= capacity){
+                    wagons[index] += passangers;
+                    break;
+                }
+            }
+        }
+    }
+    
+    console.log(wagons.join(' '));
+}
+
+train(['32 54 21 12 4 0 23', '75', 'Add 10', 'Add 0', '30', '10', '75'])
