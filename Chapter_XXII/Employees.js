ea57eac922af5fca23375e857bef4c3392ccf557
KonstantinChikov <kmpetrov7@gmail.com>
2023-11-23 01:35:54 +0200

    Objects and Classes

---
+++ Chapter_XXII/Employees.js
@@ -0,0 +1,15 @@
+// Ex. I
+
+function solve(arr){
+    for(Name of arr){
+        let employee = {name: Name, personalNum: Name.length }
+        console.log(`Name: ${employee.name} -- Personal Number: ${employee.personalNum}`);
+    }
+}
+
+solve([
+        'Silas Butler',
+        'Adnaan Buckley',
+        'Juan Peterson',
+        'Brendan Villarreal'
+     ]);
